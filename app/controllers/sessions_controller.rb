3c3ddb0d5ffb8dd120602724b9900631c2eee978
Luis G Mejia <lgm1213@gmail.com>
2025-12-04 14:52:24 -0500

    simple User login created, user model and views with just an email and password, creates/destroys sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    # Renders the login form automatically
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully!"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: "Logged out!"
+  end
+end
