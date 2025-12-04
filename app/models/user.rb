906f4f698e96823c6adcaf555ac8714373872f26
Luis G Mejia <lgm1213@gmail.com>
2025-12-04 18:03:28 -0500

    created a registration pathway way for the user and nested the userProfile in the process

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  # Handles password encryption with bcrypt gem
   has_secure_password
 
   # A user has one bio profile
   has_one :user_profile, dependent: :destroy
+
+  # Allows the User form to accept fields for the UserProfile
+  accepts_nested_attributes_for :user_profile
   
   # A user can have MANY licenses (e.g., FL and NY)
   has_many :professional_licenses, dependent: :destroy
