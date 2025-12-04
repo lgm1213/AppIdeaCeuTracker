0b6ea19168e0f611b5dee7109bc93dc1c130909d
Luis G Mejia <lgm1213@gmail.com>
2025-12-04 15:16:38 -0500

    Filled in models for intended usage foundation

--- app/models/professional_license.rb
+++ app/models/professional_license.rb
@@ -1,4 +1,23 @@
 class ProfessionalLicense < ApplicationRecord
   belongs_to :user
   belongs_to :issuing_authority
+
+  validates :license_number, presence: true
+  validates :expiration_date, presence: true
+  
+  validate :validate_license_format
+
+  private
+
+  def validate_license_format
+    # If the Authority has a specific Regex rule, check the number against it
+    if issuing_authority&.license_format_regex.present?
+      # Convert string to Regex object
+      regex_rule = Regexp.new(issuing_authority.license_format_regex)
+      
+      unless license_number.match?(regex_rule)
+        errors.add(:license_number, "does not match the required format for #{issuing_authority.name}")
+      end
+    end
+  end
 end
