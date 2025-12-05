f65f548efbfba3e855c1c31fedd1ab81d1ab286a
Luis G Mejia <lgm1213@gmail.com>
2025-12-05 11:28:45 -0500

    created a dashboard controller and view for user hub to land after login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "dashboard/index"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
