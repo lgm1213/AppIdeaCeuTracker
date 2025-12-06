9995cdeef4d53b44ffac45a43a32d9ef5a2bb069
Luis G Mejia <lgm1213@gmail.com>
2025-12-06 16:16:12 -0500

    added more routes for userProfiles, cleaned up the dashboard, and userProfile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,30 +1,37 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_profile, only: [:edit, :update, :show]
 
   def edit
-    @profile = current_user.user_profile || current_user.build_user_profile
+  end
+
+  def show
   end
 
 
   def update
-    @profile = current_user.user_profile || current_user.build_user_profile
-
     if @profile.update(profile_params)
-      redirect_to dashboard_path, notice: "Profile updated successfully!"
+      redirect_to profile_path, notice: "Profile updated successfully!"
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   private
+  
+  def set_profile
+    @profile = current_user.user_profile || current_user.build_user_profile
+  end
 
   def profile_params
     params.require(:user_profile).permit(
       :full_name, 
       :middle_initial, 
-      :last_name, 
+      :last_name,
+      :birthdate, 
       :phone_number, 
-      :address, 
+      :address_line_1,
+      :address_line_2, 
       :city, 
       :state, 
       :zip_code, 
