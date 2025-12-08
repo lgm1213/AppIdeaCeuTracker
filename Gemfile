8d7a7ebfa63c1cfdd0fb0e2960a1330b5face508
[lgm1213] <[lgm1213@gmail.com]>
2025-12-08 12:43:48 -0500

    adding reporting feature to ceu in dashboard

--- Gemfile
+++ Gemfile
@@ -42,6 +42,10 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
 
+# Reporting related gems, creation: pdfs, csvs, excels,
+gem 'prawn'
+gem 'prawn-table'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
@@ -66,3 +70,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,32 @@
 Rails.application.routes.draw do
-  get "profiles/edit"
-  get "profiles/update"
   root "home#index"
-
-  #Dashboard(Protected Area) Routes
-  get "dashboard/index", to: "dashboard#index", as: "dashboard"
-
-  #Professional License & CEU Management Routes
-  resources :professional_licenses, only: [:new, :create, :edit, :update, :destroy]
-  resources :ceus, only: [:new, :create, :edit, :update, :destroy]
-
-  #User Profile Management Routes
-  resource :profile, only: [:edit, :show, :update]
 
   #Authentication & Registration Routes
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
+  # Registration Routes
   get "/signup", to: "registrations#new", as: "signup" 
   post "/signup", to: "registrations#create"
+
+  #Dashboard(Protected Area) Routes
+  get "dashboard/index", to: "dashboard#index", as: "dashboard"
+
+  #User Profile Management Routes
+  resource :profile, only: [:edit, :show, :update]
+
+  #Professional License & CEU Management Routes
+  resources :professional_licenses, only: [:new, :create, :edit, :update, :destroy]
+  resources :ceus, only: [:new, :create, :edit, :update, :destroy]
+
+  # Reporting Routes
+  get 'reports/export', to: 'reports#export', as: 'export_report'
+
+
+
+
+
 
 
 
